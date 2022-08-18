c226e54f8da1403ec5bf9d301680bb781e225812
linxb <448923515@qq.com>
2022-08-18 13:55:00 +0800

    修改任务列表页

--- src/views/task/index/index.vue
+++ src/views/task/index/index.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="p-4">
     <BasicTable @register="registerTable">
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.key === 'id'"> ID: {{ record.id }} </template>
-        <template v-else-if="column.key === 'status'">
-          <Tag color="green">
-            {{ record.status }}
-          </Tag>
+      <template #bodyCell="{ column }">
+        <template v-if="column.key === 'status'">
+          <Switch checked-children="开" un-checked-children="关" v-model:checked="checked" />
+        </template>
+        <template v-if="column.key === 'action'">
+          <a-button @click="handleOpen" color="success"> 手工执行 </a-button>
+          <a-button type="primary" class="ml-2"> 编辑 </a-button>
+          <a-button color="warning" class="ml-2"> 查看日志 </a-button>
+          <a-button color="error" class="ml-2"> 删除 </a-button>
         </template>
       </template>
     </BasicTable>
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, ref } from 'vue';
   import { BasicTable, useTable, BasicColumn } from '/@/components/Table';
   import { getListApi } from '/@/api/task';
-  import { Tag } from 'ant-design-vue';
+  import { Switch } from 'ant-design-vue';
   const columns: BasicColumn[] = [
     {
       title: '任务ID',
       dataIndex: 'id',
+      width: 80,
       fixed: 'left',
     },
     {
@@ -34,10 +38,7 @@
     {
       title: '状态',
       dataIndex: 'status',
-    },
-    {
-      title: '备注',
-      dataIndex: 'remark',
+      width: 80,
     },
     {
       title: '创建时间',
@@ -49,17 +50,34 @@
     },
   ];
   export default defineComponent({
-    components: { BasicTable, Tag },
+    components: { BasicTable, Switch },
     setup() {
       const [registerTable] = useTable({
         title: '任务列表',
         api: getListApi,
         columns: columns,
         bordered: true,
+        actionColumn: {
+          title: '操作',
+          width: 360,
+          dataIndex: 'action',
+        },
       });
+
+      const checked = ref<boolean>(true);
+
+      function handleDelete(record: Recordable) {
+        console.log('点击了删除', record);
+      }
+      function handleOpen(record: Recordable) {
+        console.log('点击了启用', record);
+      }
 
       return {
         registerTable,
+        handleDelete,
+        handleOpen,
+        checked,
       };
     },
   });
